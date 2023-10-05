8d80bafec7dcf7d314a5e24a334b7ec17ab86ead
Duong Huynh <trungduong22021619@gmail.com>
2023-10-05 09:06:00 +0700

    First Commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,314 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const Role = require("../models/Role");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const moment = require("moment/moment");
+
+//@desc Register New user
+//@route POST /api/users/register
+//@access public
+const registerUser = asyncHandler(async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      gender,
+      dob,
+      address,
+      phone,
+      email,
+      password,
+      roleName,
+    } = req.body;
+    if (
+      firstName === undefined ||
+      lastName === undefined ||
+      gender === undefined ||
+      dob === undefined ||
+      address === undefined ||
+      phone === undefined ||
+      email === undefined ||
+      password === undefined ||
+      roleName === undefined
+    ) {
+      res.status(400);
+      throw new Error("All field not be empty!");
+    }
+    const userEmailAvailable = await User.findOne({ email });
+    if (userEmailAvailable) {
+      res.status(400);
+      throw new Error("User has already registered with Email!");
+    }
+
+    if (phone !== "") {
+      const pattern = /^0\d{9}$/;
+      if (!pattern.test(phone)) {
+        res.status(400);
+        throw new Error("Phone must have 10 numbers and start with 0 number!");
+      }
+      const userPhoneAvailable = await User.findOne({ phone });
+      if (userPhoneAvailable) {
+        res.status(400);
+        throw new Error("User has already registered with Phone Number!");
+      }
+    }
+
+    const date = new Date(dob);
+
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const role = await Role.findOne({ roleName });
+    const user = await User.create({
+      firstName,
+      lastName,
+      gender,
+      dob: dob === "" ? dob : date,
+      address,
+      phone,
+      email,
+      password: hashedPassword,
+      role_id: role._id.toString(),
+    });
+    if (!user) {
+      res.status(400);
+      throw new Error("User data is not Valid!");
+    }
+    const accessToken = jwt.sign(
+      {
+        user: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          roleName: role.roleName,
+          role_id: user.role_id,
+          imgURL: user.imgURL,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const refreshToken = jwt.sign(
+      {
+        user: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          roleName: role.roleName,
+          role_id: user.role_id,
+          imgURL: user.imgURL,
+          id: user.id,
+        },
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "7d" }
+    );
+    // Create secure cookie with refresh token
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true, //accessible only by web server
+      secure: true, //https
+      sameSite: "None", //cross-site cookie
+      maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+    });
+
+    res.status(200).json({ accessToken });
+  } catch (error) {
+    res
+      .status(res.statusCode || 500)
+      .send(error.message || "Internal Server Error");
+  }
+});
+
+const forgotPassword = asyncHandler(async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      res.status(404);
+      throw new Error("Email invalid");
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    user.otp = otp;
+    user.otpExpires = new Date();
+    await user.save();
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // use SSL
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Reset Password OTP",
+      html: `<body style="background-color:#ffffff;font-family:HelveticaNeue,Helvetica,Arial,sans-serif">
+      <table
+        align="center"
+        role="presentation"
+        cellSpacing="0"
+        cellPadding="0"
+        border="0"
+        width="100%"
+        style="max-width:37.5em;background-color:#ffffff;border:1px solid #eee;border-radius:5px;box-shadow:0 5px 10px rgba(20,50,70,.2);margin-top:20px;width:360px;margin:0 auto;padding:68px 0 130px"
+      >
+        <tr style="width:100%">
+          <td>
+            <img
+              alt="Plaid"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/640px-Eopsaltria_australis_-_Mogo_Campground.jpg"
+              width="150"
+              height="auto"
+              style="display:block;outline:none;border:none;text-decoration:none;margin:0 auto"
+            />
+            <p style="font-size:11px;line-height:16px;margin:16px 8px 8px 8px;color:#0a85ea;font-weight:700;font-family:HelveticaNeue,Helvetica,Arial,sans-serif;height:16px;letter-spacing:0;text-transform:uppercase;text-align:center">
+              Verify Your Email
+            </p>
+            <h1 style="color:#000;display:inline-block;font-family:HelveticaNeue-Medium,Helvetica,Arial,sans-serif;font-size:20px;font-weight:500;line-height:24px;margin-bottom:0;margin-top:0;text-align:center">
+              Enter the following code to finish reset your password
+            </h1>
+            <table
+              style="background:rgba(0,0,0,.05);border-radius:4px;margin:16px auto 14px;vertical-align:middle;width:280px"
+              align="center"
+              border="0"
+              cellPadding="0"
+              cellSpacing="0"
+              role="presentation"
+              width="100%"
+            >
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-size:32px;line-height:40px;margin:0 auto;color:#000;display:inline-block;font-family:HelveticaNeue-Bold;font-weight:700;letter-spacing:6px;padding-bottom:8px;padding-top:8px;width:100%;text-align:center">
+                      ${otp}
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p style="font-size:15px;line-height:23px;margin:0;color:#444;font-family:HelveticaNeue,Helvetica,Arial,sans-serif;letter-spacing:0;padding:0 40px;text-align:center">
+              Not expecting this email?
+            </p>
+            <p style="font-size:15px;line-height:23px;margin:0;color:#444;font-family:HelveticaNeue,Helvetica,Arial,sans-serif;letter-spacing:0;padding:0 40px;text-align:center">
+              Contact
+              <a
+                target="_blank"
+                style="color:#444;text-decoration:underline"
+                href="mailto:infor.driveconn@gmail.com.com"
+              >
+                infor.driveconn@gmail.com
+              </a>
+              if you did not request this code.
+            </p>
+          </td>
+        </tr>
+      </table>
+    </body>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+
+    res.status(200).json("OTP sent to email");
+  } catch (error) {
+    res
+      .status(res.statusCode || 500)
+      .send(error.message || "Internal Server Error");
+  }
+});
+
+const resetPassword = asyncHandler(async (req, res, next) => {
+  try {
+    const { email, otp } = req.body;
+    if (!email || !otp) {
+      res.status(404);
+      throw new Error("Invalid email or otp");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+    if (user.otp.toString() !== otp) {
+      res.status(400);
+      throw new Error("Wrong OTP! Please try again");
+    }
+    const currentTime = moment(new Date());
+    const otpExpires = moment(user.otpExpires);
+    const isExpired = currentTime.diff(otpExpires, "minutes");
+    if (isExpired > 10) {
+      res.status(400);
+      throw new Error("OTP is expired! Please try again");
+    }
+    const newPassword = Math.floor(100000 + Math.random() * 900000);
+    const hashedPassword = await bcrypt.hash(newPassword.toString(), 10);
+    const updateUser = await User.findByIdAndUpdate(
+      user._id,
+      {
+        password: hashedPassword,
+      },
+      { new: true }
+    );
+    if (!updateUser) {
+      res.status(500);
+      throw new Error(
+        "Something went wrong when updating new password in reset password!"
+      );
+    }
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // use SSL
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Reset Password Successfully",
+      text: `This is your new password: ${newPassword}. Please login to continue!`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+
+    res.status(200).json("Reset password successfully");
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .send(error.message || "Internal Server Error");
+  }
+});
+
+module.exports = {
+  registerUser,
+  forgotPassword,
+  resetPassword,
+};
---
+++ src/routes/UserRouter.js
@@ -0,0 +1,207 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRouter = express.Router();
+userRouter.use(bodyParser.json());
+const {
+  registerUser,
+  forgotPassword,
+  resetPassword,
+} = require("../app/controllers/UserController");
+
+/**
+ *  @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        properties:
+ *          firstName:
+ *            type: string
+ *            description: enter your first name
+ *            example: Trung
+ *          lastName:
+ *            type: string
+ *            description: enter your last name
+ *            example: Duong
+ *          gender:
+ *            type: string
+ *            description: enter your gender
+ *            example: Male
+ *          dob:
+ *            type: string
+ *            description: enter your dob
+ *            example: 22/02/2001
+ *          address:
+ *            type: string
+ *            description: enter your address
+ *            example: Ho Chi Minh
+ *          phone:
+ *            type: string
+ *            description: enter your phone
+ *            example: 0838323403
+ *          email:
+ *            type: string
+ *            description: enter your email
+ *            example: trungduong@gmail.com
+ *          password:
+ *            type: string
+ *            description: enter your password
+ *            example: 123456
+ *          role_id:
+ *            type: string
+ *            description: enter your role_id
+ *            example: Customer
+ */
+
+/**
+ * @swagger
+ * /api/users/register:
+ *  post:
+ *    tags:
+ *      - Users
+ *    summary: Register new Customer
+ *    description: Register new Customer
+ *    requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                  type: string
+ *                  description: enter your first name
+ *                  example: Trung
+ *               lastName:
+ *                  type: string
+ *                  description: enter your last name
+ *                  example: Duong
+ *               gender:
+ *                  type: string
+ *                  description: enter your gender
+ *                  example: Male
+ *               dob:
+ *                  type: string
+ *                  description: enter your dob
+ *                  example: 22/02/2001
+ *               address:
+ *                  type: string
+ *                  description: enter your address
+ *                  example: Ho Chi Minh
+ *               phone:
+ *                  type: string
+ *                  description: enter your phone
+ *                  example: 0838323403
+ *               email:
+ *                  type: string
+ *                  description: enter your email
+ *                  example: trungduong@gmail.com
+ *               password:
+ *                  type: string
+ *                  description: enter your password
+ *                  example: 123456
+ *               roleName:
+ *                  type: string
+ *                  description: enter your password
+ *                  example: Customer
+ *    responses:
+ *      201:
+ *        description:  Register Successfully!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                description:
+ *                  type: string
+ *                  example: Register Successfully!
+ *                data:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/User'
+ *      400:
+ *        description: All field not be empty! OR User has already registered with Email! OR User has already registered with Phone Number! OR User data is not Valid!
+ *
+ */
+
+userRouter.route("/register").post(registerUser);
+
+/**
+ * @swagger
+ * /api/users/forgotPassword:
+ *  post:
+ *    tags:
+ *      - Users
+ *    summary: User Forgot Password Can Get OTP to Reset
+ *    description: User Forgot Password Can Get OTP to Reset
+ *    requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                  type: string
+ *                  description: enter your email
+ *                  example: abc@gmail.com
+ *    responses:
+ *      200:
+ *        description:  Send mail OTP successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                description:
+ *                  type: string
+ *                  example: Send mail OTP successfully
+
+ *      400:
+ *        description: User not Found
+ *
+ */
+userRouter.post("/forgotPassword", forgotPassword);
+
+/**
+ * @swagger
+ * /api/users/resetPassword:
+ *  post:
+ *    tags:
+ *      - Users
+ *    summary: User verify OTP and receive new password in email
+ *    description: User verify OTP and receive new password in email
+ *    requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                  type: string
+ *                  description: enter your email
+ *                  example: abc@gmail.com
+ *               otp:
+ *                  type: string
+ *                  description: enter OTP
+ *                  example: 235122
+ *    responses:
+ *      200:
+ *        description:  Reset password successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                description:
+ *                  type: string
+ *                  example: Reset password successfully
+
+ *      400:
+ *        description: User not Found
+ *
+ */
+userRouter.post("/resetPassword", resetPassword);
+
+module.exports = userRouter;
